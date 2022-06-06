0b1376ed64956e6232d0cebab6c79f1ae15aa662
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2022-06-06 15:51:40 +0900

    Rename AT_CHECK to TORCH_CHECK
    to support higher version of CUDA and
    PyTorch

--- cuda/emd_kernel.cu
+++ cuda/emd_kernel.cu
@@ -17,6 +17,9 @@
 #define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
+#ifndef AT_CHECK
+#define AT_CHECK TORCH_CHECK
+#endif
 
 /********************************
 * Forward kernel for approxmatch
